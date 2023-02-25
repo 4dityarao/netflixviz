38e3f6a13f16d989a6082ee2e4af2afb89308e3d
Aditya Rao <skaditya@gmail.com>
2023-02-24 18:53:45 -0600

    POC with 10 movies

---
+++ netflix_viz/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from 'react';
+import NetflixViz from './NetflixViz';
+import './App.css';
+
+function App() {
+
+  return (<div className="graph">
+    <NetflixViz></NetflixViz>
+</div>);
+}
+
+export default App;
