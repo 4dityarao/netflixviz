0fbfadcb4002145c191ffd908b54a866d6d70f9e
dhwanitsharma <dhwanitsharma5@gmail.com>
2023-02-24 23:41:25 -0600

    POC in cosmos

--- netflix_viz/src/App.js
+++ netflix_viz/src/App.js
@@ -1,13 +1,45 @@
 import logo from './logo.svg';
 import React from 'react';
-import NetflixViz from './NetflixViz';
 import './App.css';
+import { Graph, GraphConfigInterface } from "@cosmograph/cosmos";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
 
-  return (<div className="graph">
-    <NetflixViz></NetflixViz>
-</div>);
+  componentDidMount() {
+    const canvas = this.canvasRef.current;
+    const config = {
+      simulation: {
+        repulsion: 0.5,
+      },
+      renderLinks: true,
+      linkColor: (link) => link.color,
+      nodeColor: (node) => {if(node.group>1){
+        return "#f54e42"
+      }
+      },
+      nodeSize: 0.5,
+      linkWidth: 0.1,
+      linkArrows: false,
+      events: {
+        onClick: (node) => {
+          console.log('Clicked node: ', node);
+        },
+      },
+      /* ... */
+    };
+    const data = require("./assets/graph_data_poc.json")
+    console.log(data)
+    const graph = new Graph(canvas, config);
+    graph.setData(data.nodes, data.links);
+  }
+
+  render() {
+    return <canvas ref={this.canvasRef} />;
+  }
 }
 
 export default App;
