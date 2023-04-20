69c6c834e2029c1834686a1581a7084562edb3b3
Aditya Rao <skaditya@gmail.com>
2023-04-19 21:58:27 -0500

    child graph working

--- netflix_viz/src/childGraph.js
+++ netflix_viz/src/childGraph.js
@@ -43,49 +43,50 @@ class ChildGraph extends React.Component {
       },
       renderLinks: true,
       linkColor: (link) => {if(link.value>1)
-      return "#c5fb61"
-      else return "#f54e42"
-      },
-      nodeColor: (node) => {
-        return "#f54e42"
-      
-      },
-      nodeSize: (node)=>{if(node.group==1){return 1}else{return 0.5}},
-      linkWidth: 0.8,
-      linkArrows: false,
-      linkVisibilityDistanceRange : [100, 1500],
-      linkVisibilityMinTransparency : 0.05,
-      scaleNodesOnZoom : true,
-      events: {
-        onClick: async (node) => {
-          if (node!== undefined){
-          // console.log('Clicked node: ', node);
-          //console.log(graph.getAdjacentNodes(node.id));
-          this.graph.selectNodeById(node.id,true);
-                  // this.state.node_id = node.id;  
-          await this.getFusedView(node)     
-          }
-          this.graph.pause();
-
+        return "#c5fb61"
+        else return "#f54e42"
         },
-        onNodeMouseOver: (node)=>{
-            // console.log('Hovered node: ', node);
+        nodeColor: (node) => {if(node.group>1){
+          return "#f54e42"
+        }
+        },
+        nodeSize: (node)=>{if(node.group==1){return 1}else{return 0.5}},
+        linkWidth: 0.8,
+        linkArrows: false,
+        linkVisibilityDistanceRange : [100, 1500],
+        linkVisibilityMinTransparency : 0.05,
+        scaleNodesOnZoom : true,
+        events: {
+          onClick:  (node) => {
+            if (node!== undefined){
+            // console.log('Clicked node: ', node);
+            //console.log(graph.getAdjacentNodes(node.id));
+            this.graph.selectNodeById(node.id,true);
+            this.sidebarRef.current.changeNodeData(node, this.graph.getAdjacentNodes(node.id).slice(1))
+            // this.state.node_id = node.id;  
+            this.childGraphRef.current.makeView(node, this.graph.getAdjacentNodes(node.id)) 
+            }
+            this.graph.pause();
+  
+          },
+          onNodeMouseOver: (node)=>{
+              // console.log('Hovered node: ', node);
+             
+          
+          },
+          onmousemove: ()=>{
+            this.graph.fitView();
+          },
+        },
            
-        
-        },
-        onmousemove: ()=>{
-          this.graph.fitView();
-        },
-      },
-      
     };
 
     this.graph = new Graph(canvas, config);
     // [this.nodes,this.links] = await this.runQuery();
     // this.graph.setData(this.nodes,this.links);
     //to run with POC data
-    const data1 = require("./assets/graph_data_poc.json")
-    this.graph.setData(data1.nodes,data1.links);
+    // const data1 = require("./assets/graph_data_poc.json")
+    // this.graph.setData(data1.nodes,data1.links);
 
     this.graph.fitView();
   };
@@ -128,14 +129,17 @@ runMovQuery = async(query)=>{
   session.close();
   //let movies = new Set()
   let movies = new Object();
+  let rec_movies = new Object();
   let links = res.records.map(r => {
     //group 1 = movie, 2= cust
-    movies[r.get("movie2")] = r.get("movie2");
+    rec_movies[r.get("movie2")] = r.get("movie2");
     //movies.add(r.get("target"))
     movies[r.get("movie1")] = r.get("movie1");
     return {"source":r.get("movie1"),"target":  r.get("movie2"),value:parseFloat(r.get("sim"))}});
   
-  let nodes = Array.from(Object.entries(movies)).map(([id,title]) =>{return {id: id,title:title, group:1}})
+  let nodes = Array.from(Object.entries(movies)).map(([id,title]) =>{return {id: id,title:"title", group:1}})
+  .concat(Array.from(Object.entries(rec_movies)).map(([id,title]) =>{return {id: id,title:"title", group:2}}))
+
               
   return [nodes,links]
 };
@@ -143,8 +147,8 @@ runMovQuery = async(query)=>{
 
 
 getQuery = async (value = "init")=>{
-  let n  =  this.nodes.filter(x=>x.group>1).map(x=>{return x.id})
-  console.log(n)
+  //let n  =  this.nodes.filter(x=>x.group>1).map(x=>{return x.id})
+  //console.log(n)
   const query_dict = {
     init: `MATCH (m:Movie)
     CALL {
@@ -178,14 +182,16 @@ highlightGraphNode = (node_id)=>{
 makeView = async(node, adjacent_nodes)=>{
   let [movienodes, simlinks] = await this.runMovQuery(`match (c:Customer{id:${node.id}})-[r:RATED]-(m)
 with distinct m as m1
-match (:Customer{id:785314})-[p:PREDICTED_RATING]-(pred_movie:Movie)
+match (:Customer{id:${node.id}})-[p:PREDICTED_RATING]-(pred_movie:Movie)
 with distinct pred_movie as m2
 MATCH (m2:Movie)-[r:similarity]-(m1:Movie)
-RETURN m1.id as movie1, m2.id as movie2, r.similarity as sim order by r.similarity desc `)
+RETURN toString(m1.id) as movie1, toString(m2.id) as movie2, r.similarity as sim order by r.similarity desc `)
 let x = movienodes.concat(adjacent_nodes)
 console.log(x)
-this.graph.setData(Array.from([{id: '14670', title: "", group: 1}]),[])
+this.graph.setData(movienodes,simlinks);
 console.log("YAY")
+// let [movienodes, simlinks] = await this.runQuery()
+// this.graph.setData(movienodes,simlinks)
 }
 render(){
   
